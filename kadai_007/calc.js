b012b02cb16fb9ac1c9f541d8bd72babfb09fe50
yokoyamasan <75236051+vaundy1104@users.noreply.github.com>
2025-06-06 03:52:33 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,11 @@
+let num = 5;
+
+if(num % 3 == 0) {
+  console.log("3の倍数です");
+} else if(num % 5 == 0) {
+  console.log("5の倍数です");
+} else if(num % 5 == 0 || num % 5 == 0) {
+  console.log("5の倍数です");
+} else {
+  console.log(num);
+}
